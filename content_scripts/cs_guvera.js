d0eeab2e7102d787b6f61feb1d388c15d2a882ff
singham007 <shreyasponkshe12345@gmail.com>
2015-05-03 17:57:06 +0530

    Guvera Search fix

--- content_scripts/cs_guvera.js
+++ content_scripts/cs_guvera.js
@@ -1,5 +1,8 @@
 Name = album = Artist1 = ImgSrc = '';
 
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
 
 var trackChangeInterval = setInterval(function() {
 	var prevName = Name;
@@ -10,7 +13,13 @@ Name= $('#player').find('.track-title a').text();
 singers=$('#player').find('.artist-name a')[0].innerText;
 commaIndex = singers.indexOf(",");
  Artist1 = (commaIndex === -1)?singers:singers.substring(0, commaIndex);
+ Artist1.capitalize();
+ Name.capitalize();
+ album.capitalize();
+ 
 ImgSrc=$('.album-thumb').attr('src');
+
+
 
 	if (Name !== prevName && Name) {
 		chrome.runtime.sendMessage( {'msg' : 'trackInfo','artist' : Artist1,'title' : Name,'album' : album,'imgsrc':ImgSrc});
